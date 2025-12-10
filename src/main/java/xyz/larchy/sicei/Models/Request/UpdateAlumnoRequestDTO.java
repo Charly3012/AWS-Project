28fba00672abfdd8e5a6e89212757636a768d48a
Charly3012 <carlocoronadosilva975@gmail.com>
2025-12-10 00:13:26 -0600

    fix: Validations on requests

--- src/main/java/xyz/larchy/sicei/Models/Request/UpdateAlumnoRequestDTO.java
+++ src/main/java/xyz/larchy/sicei/Models/Request/UpdateAlumnoRequestDTO.java
@@ -4,18 +4,25 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 import xyz.larchy.sicei.Models.Alumno;
 
 public record UpdateAlumnoRequestDTO(
+        @NotEmpty(message = "el campo 'nombre' no debe estar vacio")
         String nombres,
+
+        @NotEmpty(message = "el campo 'apellido' no debe estar vacio")
         String apellidos,
+
+        @NotEmpty(message = "el campo 'matricula' no debe estar vacio")
         String matricula,
 
+        @NotNull(message = "el campo 'promedio' no debe ser nulo")
         @Min(value = 0, message = "El promedio no puede ser negativo")
         @Max(value = 100, message = "El promedio no pude ser mayor a 100")
         Double promedio,
-
         String password,
+
         String fotoPerfilUrl
 ) {
     public Alumno toEntity(){
--- src/main/java/xyz/larchy/sicei/Models/Request/UpdateProfesorRequestDTO.java
+++ src/main/java/xyz/larchy/sicei/Models/Request/UpdateProfesorRequestDTO.java
@@ -6,10 +6,17 @@ import jakarta.validation.constraints.NotNull;
 import xyz.larchy.sicei.Models.Profesor;
 
 public record UpdateProfesorRequestDTO(
+    @NotNull(message = "el campo 'numeroEmpleado' no debe estar vacio")
     @Min(value = 0, message = "El numeroEmpleado no puede ser negativo")
     int numeroEmpleado,
+
+    @NotEmpty(message = "el campo 'nombres' no debe estar vacio")
     String nombres,
+
+    @NotEmpty(message = "el campo 'apellidos' no debe estar vacio")
     String apellidos,
+
+    @NotNull(message = "el campo 'horasClase' no debe estar vacio")
     @Min(value = 0, message = "Las horasClase no puede ser negativo")
     int horasClase
 ) {
